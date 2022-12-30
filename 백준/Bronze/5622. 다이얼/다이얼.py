bdbfdf3f7b85e9d0703cf11bf1af9e669c678f78
Haeun Choi <100997753+haeuni7@users.noreply.github.com>
2022-12-30 16:49:59 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,33 +1,10 @@
-a = {'A': 2,
- 'B': 2,
- 'C': 2,
- 'D': 3,
- 'E': 3,
- 'F': 3,
- 'G': 4,
- 'H': 4,
- 'I': 4,
- 'J': 5,
- 'K': 5,
- 'L': 5,
- 'M': 6,
- 'N': 6,
- 'O': 6,
- 'P': 7,
- 'Q': 7,
- 'R': 7,
- 'S': 7,
- 'T': 8,
- 'U': 8,
- 'V': 8,
- 'W': 9,
- 'X': 9,
- 'Y': 9,
- 'Z': 9}
+a = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+n = list(input())
 
-n = input()
 time = 0
 for i in n:
+    for j in a:
+        if i in j:
+            time += a.index(j)+3
     # 원래 수에서 1씩 더해주면 소요시간
-    time += a[i]+1
 print(time)
